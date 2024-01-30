8efa1f4181b128073f20211b26ae2558acd93c21
triduong <mtworkweb@gmail.com>
2024-01-30 22:07:42 +0700

    2665. Counter II

---
+++ 04.counter2.js
@@ -0,0 +1,24 @@
+// challenge https://leetcode.com/problems/counter-ii/
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let initialValue = init
+    return {
+        increment: () => ++initialValue,
+        decrement: () => --initialValue,
+        reset: () => {
+            initialValue = init
+            return initialValue
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
