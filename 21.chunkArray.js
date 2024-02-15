3d80e8e8eab60c136a34d943602c10cbed8fa3fd
triduong <mtworkweb@gmail.com>
2024-02-15 23:20:46 +0700

    2677. Chunk Array

---
+++ 21.chunkArray.js
@@ -0,0 +1,16 @@
+// Challenge https://leetcode.com/problems/chunk-array
+
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    let chunks = []
+
+    for (let i = 0; i < arr.length; i += size) {
+        chunks.push(arr.slice(i, i + size))
+    }
+
+    return chunks
+};
